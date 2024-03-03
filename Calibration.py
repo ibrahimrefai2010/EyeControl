6496d28ab58254d24a4503adc74da077e646c2c9
Ibrahim Refai <hemorefai2010@gmail.com>
2024-03-03 20:23:45 +0300

    Calibration

--- Calibration.py
+++ Calibration.py
@@ -8,5 +8,9 @@ root.title("EyeMouse calibration")
 root.overrideredirect(True)
 root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 
+image_path = "path/to/your/image.png"
+img = ImageTk.PhotoImage(Image.open(image_path))
+
+image_label = ctk.Label(root, image=img)
 
 root.mainloop()
