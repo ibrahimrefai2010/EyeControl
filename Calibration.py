c8dc7e212513e36a5dcc5ed4d25b6eb4133224e4
Ibrahim Refai <hemorefai2010@gmail.com>
2024-03-09 16:18:41 +0300

    Commit

--- Calibration.py
+++ Calibration.py
@@ -1,9 +1,8 @@
 def tksleep(seconds):
     from PIL import ImageTk, Image
     import tkinter as tk
-    
     ms = int(seconds * 1000)
-    root = tk._get_default_root('sleep')
+    root = root('sleep')
     var = tk.IntVar(root)
     root.after(ms, var.set, 1)
     root.wait_variable(var)
@@ -13,7 +12,7 @@ def Calibrate():
     import tkinter as tk
     import main
     #ctk.set_appearance_mode('light')
-
+    global root
     root = tk.Tk()
     root.title("EyeMouse calibration")
     root.overrideredirect(True)
@@ -22,7 +21,7 @@ def Calibrate():
     Instruction_label = tk.Label(root, text="Follow the Target with your eyes\n DO NOT move while using the app", font=("Arial", 40))
     Instruction_label.place(x=(root.winfo_screenwidth()/2 - 400), y = (root.winfo_screenheight() / 2 - 40), in_=root)
 
-    tksleep(5) #stop for 5 seconds
+    tksleep(2) #stop for 2 econds
     
     Instruction_label.destroy()
 
@@ -36,11 +35,13 @@ def Calibrate():
     photo_img = ImageTk.PhotoImage(resized_img)
 
 
-
     image_label = tk.Label(root, image=photo_img)
-    image_label.place(x=10, y=10, in_=root)
+    image_label.place(x=((root.winfo_screenwidth() / 2) - (size / 2)), y=(size / 2), in_=root)
 
-
-
-    root.mainloop()
-Calibrate()
+    tksleep(2.5)
+    #main.store_Reading()
+    tksleep(2.5)
+    
+    root.destroy()
+    root.after()
+#Calibrate()
--- main.py
+++ main.py
@@ -2,16 +2,29 @@ import cv2
 import mediapipe as mp
 import pyautogui
 import Calibration
+import threading
+
+CalibrationThread = threading.Thread(target=Calibration.Calibrate)
+CalibrationThread.start()
+
+'''
+def store_Reading():
+    reading_x_max = None
+    reading_x_min = None
+    reading_y_max = None
+    reading_y_min = None
+    if reading_x_max == None:
+        reading_x_max = x
+    elif reading_y_max == None:
+        reading_y_max = y
+    elif reading_x_min == None:
+        reading_x_min = x
+    elif reading_y_min == None:
+        reading_y_min = y
+
 cam = cv2.VideoCapture(0)
 face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 screen_w, screen_h = pyautogui.size()
-x = 0
-y = 0
-
-def StoreReading():
-    pass
-
-Calibration.Calibrate()
 while True:
     _, frame = cam.read()
     frame = cv2.flip(frame, 1)
@@ -20,6 +33,23 @@ while True:
     landmark_points = output.multi_face_landmarks
     frame_h, frame_w, _ = frame.shape
     if landmark_points:
+        landmarks = [landmark_points[0].landmark[443], landmark_points[0].landmark[474]]
+        global x
+        global y
+        x = (landmarks[0].x - landmarks[1].x)
+        y = (landmarks[0].x - landmarks[1].x)
+        print(f"x: {x}\ny: {y}")
         
+        left = [landmarks[145], landmarks[159]]
+        for landmark in left:
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame, (x, y), 3, (0, 255, 255))
+        if (left[0].y - left[1].y) < 0.004:
+            pyautogui.click()
+            pyautogui.sleep(1)
+            
     cv2.imshow('Eye Controlled Mouse', frame)
     cv2.waitKey(1)
+
+'''
