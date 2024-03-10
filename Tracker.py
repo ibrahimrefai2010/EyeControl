28708495403b0bf3f9ab74caad2a4bfd3e663bd4
Ibrahim Refai <hemorefai2010@gmail.com>
2024-03-11 00:36:30 +0300

    mi

--- Tracker.py
+++ Tracker.py
@@ -31,6 +31,14 @@ def run():
     import pyautogui
     import ctypes
     
+    global position
+    postion = 0
+    KeyboardStr = "abcdefghijklnmopqrstuvwxyz0123456789"
+    Keyboard = []
+    for Char in KeyboardStr:
+        Keyboard.append(Char)
+        print(Char)
+    print(Keyboard, "\n", Keyboard[postion])
     
     cam = cv2.VideoCapture(0)
     face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
@@ -43,7 +51,7 @@ def run():
         landmark_points = output.multi_face_landmarks
         frame_h, frame_w, _ = frame.shape
         if landmark_points:
-            landmarks = [landmark_points[0].landmark[443], landmark_points[0].landmark[474]]
+            landmarks = [landmark_points[0].landmark[450], landmark_points[0].landmark[477]]
             global x
             global y
             x = (landmarks[1].x - landmarks[0].x)
@@ -54,34 +62,48 @@ def run():
                 x_range = reading_x_max - reading_x_min
                 x_distance = x - reading_x_min
                 x_percentage = (x_distance / x_range)
+                
                 y_range = reading_y_max - reading_y_min
                 y_distance = y - reading_y_min
                 y_percentage = (y_distance / y_range)
+                
                 x_pos = (screen_w * x_percentage)
                 y_pos = (screen_h * y_percentage)
                 #y_pos = 600
+                
                 pyautogui.FAILSAFE = False
                 pyautogui.moveTo(x_pos, y_pos)
                 #print(f"x:{x_percentage * 100}\ny:{y_percentage * 100}")
                 #print(f"x: {x}   y: {y}")
                 #print(y_percentage, x_percentage, sep="\n")
+                '''
                 for landmark in landmarks:
                     x = int(landmark.x * frame_w)
                     y = int(landmark.y * frame_h)
                     cv2.circle(frame, (x, y), 3, (0, 255, 255))
+                '''
             except ZeroDivisionError:
-                pass
+                print(Exception)
 
-        '''
-            left = [landmarks[145], landmarks[159]]
+            CurrentChar = ''
+            left = [landmark_points[0].landmark[145], landmark_points[0].landmark[159]]
             for landmark in left:
                 x = int(landmark.x * frame_w)
                 y = int(landmark.y * frame_h)
                 cv2.circle(frame, (x, y), 3, (0, 255, 255))
             if (left[0].y - left[1].y) < 0.004:
-                pyautogui.click()
-                pyautogui.sleep(1)
-        '''
+                postion -= 1
+                Current_char = Keyboard[postion]
+                print(Current_char)
+            
+            right = [landmark_points[0].landmark[374], landmark_points[0].landmark[386]]
+            for landmark in right:
+                x = int(landmark.x * frame_w)
+                y = int(landmark.y * frame_h)
+                cv2.circle(frame, (x, y), 3, (0, 255, 255))
+            if (right[0].y - right[1].y) < 0.004:
+                postion += 1
+                Current_char = Keyboard[postion]
+                print(Current_char)
         cv2.imshow('Eye Controlled Mouse', frame)
         cv2.waitKey(1)
-
